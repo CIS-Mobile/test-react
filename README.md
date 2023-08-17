fb484d47c2a793f06da5885e0869f313f9c9712e
EduardK <ekleiner@cismobile.com>
2023-08-17 18:23:23 -0400

    updated Readme. Added Dockerfile. WIP on Auth

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 <h3 id="npminstall"><code>npm install</code></h3>
 
+<h3 id="updateEnv">rename the example.env file to '.env' and update the mongodb params found inside</h3>
+<p>The default mongodb credentials provided should work, if you changed these defaults upon install, and you're unsure how to configure your mongo credentials, feel free to ask</p>
+
 <h3 id="npmstart"><code>npm start</code></h3>
 
 <h4>IF you're getting an error try <code> npm run start_safe</code> instead</h4>
--- src/app/auth/AuthGuard.jsx
+++ src/app/auth/AuthGuard.jsx
@@ -1,46 +1,13 @@
-import useAuth from 'app/hooks/useAuth';
-// import { flat } from 'app/utils/utils';
-import { Navigate, useLocation } from 'react-router-dom';
-// import AllPages from '../routes';
-
-// const userHasPermission = (pathname, user, routes) => {
-//   if (!user) {
-//     return false;
-//   }
-//   const matched = routes.find((r) => r.path === pathname);
-
-//   const authenticated =
-//     matched && matched.auth && matched.auth.length ? matched.auth.includes(user.role) : true;
-//   return authenticated;
-// };
+import useAuth from "app/hooks/useAuth";
+import { Navigate, useLocation } from "react-router-dom";
 
 const AuthGuard = ({ children }) => {
-  let {
-    isAuthenticated,
-    // user
-  } = useAuth();
-  const { pathname } = useLocation();
+	const { isAuthenticated } = useAuth();
+	const { pathname } = useLocation();
 
-  //   const routes = flat(AllPages);
+	if (isAuthenticated) return <>{children}</>;
 
-  //   const hasPermission = userHasPermission(pathname, user, routes);
-  //   let authenticated = isAuthenticated && hasPermission;
-
-  // // IF YOU NEED ROLE BASED AUTHENTICATION,
-  // // UNCOMMENT ABOVE LINES
-  // // AND COMMENT OUT BELOW authenticated VARIABLE
-
-  let authenticated = isAuthenticated;
-
-  return (
-    <>
-      {authenticated ? (
-        children
-      ) : (
-        <Navigate replace to="/session/signin" state={{ from: pathname }} />
-      )}
-    </>
-  );
+	return <Navigate replace to="/session/signin" state={{ from: pathname }} />;
 };
 
 export default AuthGuard;
